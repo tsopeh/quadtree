a10cbc0abca3b055b353d91211040064cb02c0ec
Petar Stokić <5ar100kic@gmail.com>
2023-05-01 22:17:38 +0200

    init p5

--- packages/quadtree/src/main.ts
+++ packages/quadtree/src/main.ts
@@ -1,1 +1,13 @@
-import './app/app.element.ts'
+import p5 from 'p5'
+
+export const sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(400, 400)
+  }
+
+  p.draw = () => {
+    p.background(220)
+    p.ellipse(50, 50, 80, 80)
+  }
+}
+const app = new p5(sketch, document.body)
